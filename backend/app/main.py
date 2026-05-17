5ab6bdc44c153462c30dce2dc2cf99dcd4e45af1
Anas Warsi <anaswarsi51@gmail.com>
2026-05-17 15:02:37 +0000

    Add new frontend URL to CORS settings

--- backend/app/main.py
+++ backend/app/main.py
@@ -57,6 +57,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:5174",
         "https://gridsense-868ae1lao-anas-warsis-projects.vercel.app/",
+        "https://gridsense-ixfuerocg-anas-warsis-projects.vercel.app/",
         "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
